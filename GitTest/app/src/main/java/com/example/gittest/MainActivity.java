3c832ed0456a8fb938bd4fccbf0b5ab2502d5c93
汤冯涛William <63336705+William3365@users.noreply.github.com>
2020-08-30 15:39:25 +0800

    第二次测试
    测试目的，能否对单一文件按进行修改

--- GitTest/app/src/main/java/com/example/gittest/MainActivity.java
+++ GitTest/app/src/main/java/com/example/gittest/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        第一个正式git-Android测试
+
+//        这是第二次测试，第一次测试结果可以放进旧仓库
     }
 }
