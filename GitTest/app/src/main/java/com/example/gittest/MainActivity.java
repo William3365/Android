3477715e361635e97bdcc9da47379f0c8f02b68b
汤冯涛William <63336705+William3365@users.noreply.github.com>
2020-08-30 15:37:23 +0800

    第一个git-Android测试
    正式测试01，实验目的是新建项目能否放入旧仓库

---
+++ GitTest/app/src/main/java/com/example/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        第一个正式git-Android测试
+    }
+}
